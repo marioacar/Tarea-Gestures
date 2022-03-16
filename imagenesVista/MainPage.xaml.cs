bc8282e719e899a4ceb6e133d7dbe70d34d1054b
marioacar <mcarvantes@ucol.mx>
2022-03-15 23:53:18 -0600

    Add project files.

---
+++ imagenesVista/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace imagenesVista
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+
+        
+
+        async void OnTapGestureRecognizerTapped(Object sender, EventArgs args)
+        {
+
+            
+            {
+               var ima = (Image)sender;
+
+
+                await this.Navigation.PushModalAsync(new Page1(ima));
+            }
+           
+        }
+
+        
+
+    }
+
+}
---
+++ imagenesVista/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace imagenesVista
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1(object sender)
+        {
+            InitializeComponent();
+            var imagen = (Image)sender;
+            x1.Source = imagen.Source;
+        }
+
+
+
+        void Pinch(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            
+            this.Navigation.PopModalAsync();
+        }
+    }
+}
